377ea4f28dc1f30a13f3bed80b622a29d62182d8
Simon Boerwinkle <sboerwinkle@gmail.com>
2014-01-31 23:08:02 -0600

    A broken include!

--- main.c
+++ main.c
@@ -1,7 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <GL/glfw.h>
 
 int main(int argc, char** argv){
 	puts("Hello world!");
+	if(!glfwInit()) puts("Oh no!");
+	else puts("Oh yeah!");
 	return 0;
 }
