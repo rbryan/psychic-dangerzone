04eb380374022af28959a99876837f8643cabf33
russell bryan <bryanr@asmsa.org>
2014-02-02 15:45:33 -0600

    windows now with 20 percent less derp

--- main.c
+++ main.c
@@ -2,17 +2,19 @@
 #include <stdlib.h>
 #include <SDL2/SDL.h>
 
-SDL_Window* winderp;
-SDL_Renderer* render;
-SDL_Texture* texture;
-SDL_GLContext glCon;
 
 int main(int argc, char** argv){
+	
+	SDL_Window* window;
+	SDL_Renderer* render;
+	SDL_Texture* texture;
+	SDL_GLContext glCon;
+
 	SDL_Init(SDL_INIT_VIDEO | SDL_INIT_EVENTS);
-	winderp = SDL_CreateWindow("Behold OpenGL", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 750, 750, 0);
-	render = SDL_CreateRenderer(winderp, -1, 0);
+	window = SDL_CreateWindow("Behold OpenGL", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 750, 750, 0);
+	render = SDL_CreateRenderer(window, -1, 0);
 	texture = SDL_CreateTexture(render, SDL_PIXELFORMAT_ARGB8888, SDL_TEXTUREACCESS_STREAMING, 750, 750);
-	glCon = SDL_GL_CreateContext(winderp);
+	glCon = SDL_GL_CreateContext(window);
 	SDL_Event event = {0};
 	while(event.type!=SDL_QUIT)
 		SDL_WaitEvent(&event);
