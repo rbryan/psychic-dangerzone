7e1c681985b0385db563c00ab23627f759cc77d3
Simon Boerwinkle <sboerwinkle@gmail.com>
2014-02-02 15:46:26 -0600

    Working on drawing a line

--- main.c
+++ main.c
@@ -1,18 +1,22 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <SDL2/SDL.h>
+#include <GL/gl.h>
 
 SDL_Window* winderp;
-SDL_Renderer* render;
-SDL_Texture* texture;
+//SDL_Renderer* render;
+//SDL_Texture* texture;
 SDL_GLContext glCon;
 
 int main(int argc, char** argv){
 	SDL_Init(SDL_INIT_VIDEO | SDL_INIT_EVENTS);
 	winderp = SDL_CreateWindow("Behold OpenGL", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 750, 750, 0);
-	render = SDL_CreateRenderer(winderp, -1, 0);
-	texture = SDL_CreateTexture(render, SDL_PIXELFORMAT_ARGB8888, SDL_TEXTUREACCESS_STREAMING, 750, 750);
+//	render = SDL_CreateRenderer(winderp, -1, 0);
+//	texture = SDL_CreateTexture(render, SDL_PIXELFORMAT_ARGB8888, SDL_TEXTUREACCESS_STREAMING, 750, 750);
 	glCon = SDL_GL_CreateContext(winderp);
+	GLvoid data[] = {0xFF, 0xFF, 0xFF, 0xFF, 0, 0, 200, 200};
+	glDrawElements(GL_LINES, 1, GL_UNISGNED_BYTE, data);
+	SDL_GL_SwapWindow(winderp);
 	SDL_Event event = {0};
 	while(event.type!=SDL_QUIT)
 		SDL_WaitEvent(&event);
