711850be97a1145527df024e7625ede54e669a3a
Simon Boerwinkle <sboerwinkle@gmail.com>
2014-02-02 15:31:19 -0600

    Made a gl context

--- main.c
+++ main.c
@@ -5,12 +5,14 @@
 SDL_Window* winderp;
 SDL_Renderer* render;
 SDL_Texture* texture;
+SDL_GLContext glCon;
 
 int main(int argc, char** argv){
 	SDL_Init(SDL_INIT_VIDEO | SDL_INIT_EVENTS);
 	winderp = SDL_CreateWindow("Behold OpenGL", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 750, 750, 0);
 	render = SDL_CreateRenderer(winderp, -1, 0);
 	texture = SDL_CreateTexture(render, SDL_PIXELFORMAT_ARGB8888, SDL_TEXTUREACCESS_STREAMING, 750, 750);
+	glCon = SDL_GL_CreateContext(winderp);
 	SDL_Event event = {0};
 	while(event.type!=SDL_QUIT)
 		SDL_WaitEvent(&event);
